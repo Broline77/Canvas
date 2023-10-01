23daaf0c20274692da5a85062bb762aa1c7e99e7
broline <brolineoronu3@gmail.com>
2023-10-01 17:28:23 +0300

    clear comments

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
 #import the path function from djangos url module used to define url patterns
 from django.urls import path
-#imports the views model form the current dir
 from . import views
 
-#a listthat defines url patterns for your django project
 #a list of 'path' objects that map URLS to view functions
 #''empty string reps the base url/root url of your website/home
 #views.index --> the view function that should handlle requests to this url
